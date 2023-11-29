6d172665c0190f2584f31ece9213c2da13300e24
kharateameya <kharateameya@gmail.com>
2023-11-29 18:49:44 +0530

    installed reactjs and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+     <h1 className="text-blue-400 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
